333c05e2213715b3da1aa3ef31fa5c29587e01d4
Martin Lexow <martin.lexow@ixeau.com>
2022-07-12 12:05:21 +0200

    init

--- TransparentWKWebView/ContentView.swift
+++ TransparentWKWebView/ContentView.swift
@@ -1,21 +1,69 @@
-//
-//  ContentView.swift
-//  TransparentWKWebView
-//
-//  Created by Martin Lexow on 12.07.22.
-//
+
 
 import SwiftUI
+import WebKit
+
 
 struct ContentView: View {
+    
+    let url = Bundle.main.url(forResource: "demo", withExtension: "html")!
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return ZStack {
+            WKWebViewRepresentable(url: self.url)
+                .padding(12.0)
+        }
+        .frame(width: 400.0, height: 400.0)
+        .background {
+            Color.green
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+public struct WKWebViewRepresentable: NSViewRepresentable {
+    
+    public init(url: URL) {
+        self.url = url
+    }
+    
+    let url: URL
+    
+    private let configuration: WKWebViewConfiguration = {
+        let config = WKWebViewConfiguration()
+        config.setValue(false, forKey: "drawsBackground")
+        return config
+    }()
+    
+    public func makeNSView(context: Context) -> WKWebView {
+        
+        let webView = TransparentWKWebView(frame: CGRect.zero, configuration: self.configuration)
+        
+        webView.setValue(true, forKey: "drawsTransparentBackground")
+        webView.setValue(false, forKey: "drawsBackground")
+        
+        webView.wantsLayer = true
+        webView.layer?.backgroundColor = NSColor.clear.cgColor
+        
+        webView.underPageBackgroundColor = NSColor.clear
+        webView.enclosingScrollView?.backgroundColor = NSColor.clear
+        
+        return webView
+        
+    }
+    
+    
+    public func updateNSView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: self.url)
+        webView.load(request)
+    }
+    
+    
+}
+
+
+final class TransparentWKWebView: WKWebView {
+    override var isOpaque: Bool {
+        return false
     }
 }
