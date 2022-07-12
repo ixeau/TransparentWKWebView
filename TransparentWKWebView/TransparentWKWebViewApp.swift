5a99e97c1d34f9d143622f57a388ff1c3e812bbf
Martin Lexow <martin.lexow@ixeau.com>
2022-07-12 11:53:12 +0200

    Initial Commit

---
+++ TransparentWKWebView/TransparentWKWebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransparentWKWebViewApp.swift
+//  TransparentWKWebView
+//
+//  Created by Martin Lexow on 12.07.22.
+//
+
+import SwiftUI
+
+@main
+struct TransparentWKWebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
